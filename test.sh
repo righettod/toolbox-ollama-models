c354690a4e8413246764ba7f77be13412ec9a545
righettod <dominique.righetto@gmail.com>
2026-05-16 18:52:23 +0200

    first commit

--- test.sh
+++ test.sh
@@ -2,9 +2,9 @@
 # Script to test all model files
 echo "[+] Start ollama server..."
 pgrep -x ollama > /dev/null || ollama serve &
-#for i in {1..10}; do
-#  curl -sf http://localhost:11434 && break
-#  sleep 2
+for i in {1..10}; do
+  curl -sf http://localhost:11434 && break
+  sleep 2
 #done
 for file in $(find . -type f -name Modelfile)
 do
