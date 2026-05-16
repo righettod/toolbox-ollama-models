8a1b0de8c84b9baf4198994ddedfa9d2ff641d6f
righettod <dominique.righetto@gmail.com>
2026-05-16 18:54:24 +0200

    first commit

--- test.sh
+++ test.sh
@@ -6,8 +6,11 @@ for i in {1..10}; do
   curl -sf http://localhost:11434 && break
   sleep 2
 done
+echo "[+] Build every models..."
 for file in $(find . -type f -name Modelfile)
 do
   model_name=$(basename $(dirname "$file"))
   ollama create $model_name -f $file
 done
+echo "[+] Information"
+ollama list
